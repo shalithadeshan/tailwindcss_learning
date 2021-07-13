8c5b1885ac855b6506d83ca110675029e55e74b5
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-07-13 23:36:25 +0530

    create simple web page using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF6363',
+                secondary: {
+                    100: '#E2E2D5',
+                    200: '#888883',
+                }
+            },
+            fontFamily: {
+                body: ['Nunito'],
+
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
