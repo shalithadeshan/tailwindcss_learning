0984d8cdb083a415f6f2d1589decd747de60f093
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-07-14 13:53:25 +0530

    add dark theme to web site

--- public/index.js
+++ public/index.js
@@ -1,10 +1,24 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 
+const toggle = document.querySelector('#toggle');
+
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
+    }
+})
+
+toggle.addEventListener('click', () => {
+    let htmlClasses = document.querySelector('html').classList;
+    if (localStorage.theme == 'dark') {
+        htmlClasses.remove('dark');
+        // localStorage.removeItem('theme');
+        localStorage.theme = 'light'
+    } else {
+        htmlClasses.add('dark');
+        localStorage.theme = 'dark';
     }
 })
