c38dbe85d9480c3206541b1eb40e624b88be7210
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-07-19 17:46:13 +0530

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./public/index.html
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    purge: [],
+    purge: {
+        content: ['./public/**/*.html']
+    },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
